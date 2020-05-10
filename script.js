470e126b2397ed70c7ca98a3d941b1cb8fc697cc
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-05-10 12:53:23 +0400

    Configured Tab size to 2

--- script.js
+++ script.js
@@ -12,60 +12,60 @@ calculateButton.addEventListener("click", getResult);
 // This function puts everything together and runs whole app:
 
 function getResult() {
-    cv = initialAmountInput.value;
-    tp = topUpInput.value;
-    r = interestRateInput.value;
-    d = daysInput.value;
-    if (toCheck(cv, tp, r, d)) {
-        alert(toCalculate(cv, tp, r, d));
-    } else {
-        return NaN;
-    }
+  cv = initialAmountInput.value;
+  tp = topUpInput.value;
+  r = interestRateInput.value;
+  d = daysInput.value;
+  if (toCheck(cv, tp, r, d)) {
+    alert(toCalculate(cv, tp, r, d));
+  } else {
+    return NaN;
+  }
 }
 
 // This function valides input fields:
 
 function toCheck(cv, tp, r, d) {
-    if (cv <= 0 || cv === '') {
-        errorText.innerHTML = '<span class="error-logo">i</span> Вы не правильно ввели "Начальную сумму вклада"!';
-        errorText.className = "";
-        console.warn('Начальную сумму вклада введен неправильно');
-        return NaN;
-    }
-    if (tp < 0 || tp === '') {
-        errorText.innerHTML = '<span class="error-logo">i</span> Вы не правильно ввели "Сумму ежемесячного пополнения"!';
-        errorText.className = '';
-        console.warn('Сумму ежемесячного пополнени введен неправильно');
-        return NaN;
-    }
-    if (r <= 0 || r > 100 || r === '') {
-        errorText.innerHTML = '<span class="error-logo">i</span> Процентная ставка может быть только от 0 до 100!';
-        errorText.className = '';
-        console.warn('Процентная ставка введен неправильно');
-        return NaN;
-    }
-    if (d <= 0 || d === '') {
-        errorText.innerHTML = '<span class="error-logo">i</span> Вы не правильно ввели "Срок вклада"!';
-        errorText.className = '';
-        console.warn('Срок вклада введен неправильно');
-        return NaN;
-    }
-    errorText.className = "input-error";
-    return true;
+  if (cv <= 0 || cv === '') {
+    errorText.innerHTML = '<span class="error-logo">i</span> Вы не правильно ввели "Начальную сумму вклада"!';
+    errorText.className = "";
+    console.warn('Начальную сумму вклада введен неправильно');
+    return NaN;
+  }
+  if (tp < 0 || tp === '') {
+    errorText.innerHTML = '<span class="error-logo">i</span> Вы не правильно ввели "Сумму ежемесячного пополнения"!';
+    errorText.className = '';
+    console.warn('Сумму ежемесячного пополнени введен неправильно');
+    return NaN;
+  }
+  if (r <= 0 || r > 100 || r === '') {
+    errorText.innerHTML = '<span class="error-logo">i</span> Процентная ставка может быть только от 0 до 100!';
+    errorText.className = '';
+    console.warn('Процентная ставка введен неправильно');
+    return NaN;
+  }
+  if (d <= 0 || d === '') {
+    errorText.innerHTML = '<span class="error-logo">i</span> Вы не правильно ввели "Срок вклада"!';
+    errorText.className = '';
+    console.warn('Срок вклада введен неправильно');
+    return NaN;
+  }
+  errorText.className = "input-error";
+  return true;
 }
 
 // This function calculates compound interest rate:
 
 function toCalculate(cv, tp, r, d) {
-    if (d < 30) {
-        return "Future value of deposit will be: " + +cv;
-    } else {
-        let months = Math.trunc(d / 30);
-        for (i = 1; i <= months; i++) {
-            cv = +cv + +tp + (+cv * +r) / 1200;
-        }
+  if (d < 30) {
+    return "Future value of deposit will be: " + +cv;
+  } else {
+    let months = Math.trunc(d / 30);
+    for (i = 1; i <= months; i++) {
+      cv = +cv + +tp + (+cv * +r) / 1200;
     }
-    cv -= +tp;
-    return "Future value of deposit will be: " + +cv.toFixed(2);
+  }
+  cv -= +tp;
+  return "Future value of deposit will be: " + +cv.toFixed(2);
 }
 
